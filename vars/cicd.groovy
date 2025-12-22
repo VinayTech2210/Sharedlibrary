68eb356e0015c850b1ffee41f29a924d89b230a0
VinayTech2210 <vinaykandregula10@gmail.com>
2025-12-22 11:44:40 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def gitbuildArtifact()
 {
   sh 'mvn package'
 }
+def DeployementTomcat(jobname,ipaddress,contextpath)
+{
+sh "scp /var/lib/jenkins/workspace/${jobname}/webapp//target/webapp.war azureuser@${ipaddress}/var/lib/tomcat10/webapps/${contextapp}.war"
+}
